d6512e0d8fb372187d6a3d7ba7a2b066926fe266
digitalmockingbird <noah@digitalmockingbird.com>
2014-12-06 15:46:41 -0800

    backing up

--- eulathingy/dashboard/views.py
+++ eulathingy/dashboard/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-
-
     return render(request, 'dashboard/index.html')
+
+def contact(request):
+    return render(request, 'dashboard/contact.html')
+
+def app(request):
+    return render(request, 'dashboard/app.html')
--- eulathingy/urls.py
+++ eulathingy/urls.py
@@ -10,6 +10,9 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^$', lambda r : HttpResponseRedirect('dashboard/')),
+    url(r'^index/', lambda r : HttpResponseRedirect('dashboard/')),
+    url(r'^app/', lambda r : HttpResponseRedirect('dashboard/app')),
+    url(r'^contact/', lambda r : HttpResponseRedirect('dashboard/contact')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^dashboard/', include('dashboard.urls', namespace='dashboard')),
 )
