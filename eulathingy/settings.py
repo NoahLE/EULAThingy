721db35e7a4b2c2eeee4e41bfa71e157f3d850e7
Liam Costello <liam@transifex.com>
2014-12-06 13:23:56 +0200

    Removed host from DB params

--- eulathingy/settings.py
+++ eulathingy/settings.py
@@ -61,7 +61,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'eulathingy',
         'USER': 'postgres',
-        'HOST': '127.0.0.1',
         'PORT': 5432,
     }
 }
