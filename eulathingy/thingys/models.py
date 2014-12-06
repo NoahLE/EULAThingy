f840e6460713e19b361e8dc238fbe84dd7bbe973
Liam Costello <liam@transifex.com>
2014-12-06 14:45:49 +0200

    Added initial models for Thingy and ThingySections

--- eulathingy/thingys/models.py
+++ eulathingy/thingys/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Thingy(models.Model):
+    """
+    A high-level object to hold information on an upload of a Thingy.
+
+    A Thingy is defined as:
+        * terms and conditions
+        * EULAs
+        * Anything with an enormous amount of text and overly complex
+        language.
+    """
+    name = models.CharField(
+        max_length=100, null=False, blank=False
+    )
+    summary = models.CharField(
+        max_length=200, null=True, blank=False
+    )
+    uploaded_on = models.DateTimeField(
+        auto_now_add=True, null=False, blank=False
+    )
+
+
+class ThingySection(models.Model):
+    """
+    A section of a Thingy's content. Thingys are broken up into sections
+    which are then serialised into the DB. The parser will remove data that
+    isn't required and attempt to keep only that which is useful.
+    """
+    section_name = models.CharField(
+        max_length=50, null=True, blank=False
+    )
+    content = models.TextField(
+        null=False, blank=False
+    )
+    thingy = models.ForeignKey(Thingy)
