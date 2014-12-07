ae933c2229902a801f16210aed0eec199e9df71b
Liam Costello <liam@transifex.com>
2014-12-08 00:39:05 +0200

    Getting tired now...

--- eulathingy/urls.py
+++ eulathingy/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf import settings
 
-from eulathingy.uploads.views import upload_nginx
+from uploads.views import upload_nginx
 
 
 admin.autodiscover()
