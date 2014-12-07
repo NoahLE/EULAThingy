df68058f84f059b5b1af4180f68677918aa6e296
digitalmockingbird <noah@digitalmockingbird.com>
2014-12-07 00:03:02 -0800

    small html updates

--- eulathingy/dashboard/views.py
+++ eulathingy/dashboard/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.template import RequestContext
 
 
 # Create your views here.
 def index(request):
+    context = RequestContext(request)
+
+
+
     return render(request, 'dashboard/index.html')
 
 def contact(request):
