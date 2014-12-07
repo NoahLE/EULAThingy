0c75b4964df214484945eee896ffbbcd90fe8f6b
Liam Costello <liam@transifex.com>
2014-12-08 01:56:41 +0200

    ...

--- eulathingy/uploads/views.py
+++ eulathingy/uploads/views.py
@@ -1,7 +1,9 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def upload(request):
         return render(request, 'uploads/upload.html')
 
