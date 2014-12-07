c534c32368665a24137e184bc0b3fb0473d5f74f
Liam Costello <liam@transifex.com>
2014-12-07 20:16:29 +0200

    Added static URL

--- eulathingy/urls.py
+++ eulathingy/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf import settings
 
 admin.autodiscover()
 
@@ -13,4 +15,4 @@ urlpatterns = patterns('',
                                 namespace='dashboard')),
     url(r'^uploads/', include('uploads.urls',
                               namespace='uploads'))
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
