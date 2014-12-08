d78f1a5af89d617e592a1b47fd30c18d6ab1872d
Liam Costello <liam@transifex.com>
2014-12-08 04:25:01 +0200

    Lots of stuff

--- eulathingy/uploads/parsers/procedures.py
+++ eulathingy/uploads/parsers/procedures.py
@@ -14,7 +14,9 @@ def generate_strings(file_path, file_name, thingy):
     reader = PlaintextCorpusReader(file_path, [file_name])
     sentences = reader.sents()
     for sentence in sentences:
-        yield ThingyString(
-            doc=thingy,
-            string=(' '.join(word for word in sentence))
-        )
+        string = (' '.join(word for word in sentence))
+        if string != '':
+            yield ThingyString(
+                doc=thingy,
+                string=string
+            )
--- eulathingy/uploads/views.py
+++ eulathingy/uploads/views.py
@@ -1,4 +1,5 @@
 from uuid import uuid4
+from django.db import transaction
 
 from django.db.models.loading import get_model
 from django.http.response import HttpResponse
@@ -19,9 +20,13 @@ def upload_nginx(request):
     uploaded_file = request.FILES['upload_file']
     filename, path = _handle_file(uploaded_file)
     doc = ThingyDoc(
-        title='lol',
+        title=request.POST.get('title', 'We will never know :-('),
     )
-    strings = list(procedures.generate_strings(path, filename, doc))
+    doc.save()
+    with transaction.commit_on_success():
+        strings = procedures.generate_strings(path, filename, doc)
+        for string in strings:
+            string.save()
 
     return HttpResponse(str(request.FILES))
 
