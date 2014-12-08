c996203c88d33c51a2cb5ffc5cd9356d3c970f3f
Liam Costello <liam@transifex.com>
2014-12-08 02:34:33 +0200

    Remove dupes

--- eulathingy/settings.py
+++ eulathingy/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = (
     'thingys',
     'uploads',
     'dashboard',
-    'uploads',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -79,10 +78,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-ES_HOSTS = ['127.0.0.1']
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
 
